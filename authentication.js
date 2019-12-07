aec06240baab058c13148939dffdb472b7465cb9
jboe26 <joshrboepple@outlook.com>
2019-12-07 10:12:17 -0500

    firebase

--- authentication.js
+++ authentication.js
@@ -1,30 +1,117 @@
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  })
+var firebase = require('firebase');
+var firebaseui = require('firebaseui');
 
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  })
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      var userName = user.userName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      // ...
-    } else {
-      // User is signed out.
-      
-    }
-  })
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    
+  });
+
+  ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+      {
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        requireDisplayName: false
+      }
+    ]
+  });
+
+  ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+      {
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+      }
+    ],
+    
+  });
+
+  // Is there an email link sign-in?
+if (ui.isPendingRedirect()) {
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }
+  // This can also be done via:
+  if ((firebase.auth().isSignInWithEmailLink(window.location.href))) {
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }
+
+  ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+      {
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
+        // Allow the user the ability to complete sign-in cross device,
+        // including the mobile apps specified in the ActionCodeSettings
+        // object below.
+        forceSameDevice: false,
+        // Used to define the optional firebase.auth.ActionCodeSettings if
+        // additional state needs to be passed along request and whether to open
+        // the link in a mobile app if it is installed.
+        emailLinkSignIn: function() {
+          return {
+            // Additional state showPromo=1234 can be retrieved from URL on
+            // sign-in completion in signInSuccess callback by checking
+            // window.location.href.
+            url: 'https://www.example.com/completeSignIn?showPromo=1234',
+            // Custom FDL domain.
+            dynamicLinkDomain: 'example.page.link',
+            // Always true for email link sign-in.
+            handleCodeInApp: true,
+            // Whether to handle link in iOS app if installed.
+            iOS: {
+              bundleId: 'com.example.ios'
+            },
+            // Whether to handle link in Android app if opened in an Android
+            // device.
+            android: {
+              packageName: 'com.example.android',
+              installApp: true,
+              minimumVersion: '12'
+            }
+          };
+        }
+      }
+    ]
+  });
+
+
+  // Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+// // <!-- The surrounding HTML is left untouched by FirebaseUI. Your app may use that space for branding, controls and other customizations. -->
+// <h1>Welcome to My Awesome App</h1>
+// <div id="firebaseui-auth-container"></div>
+// <div id="loader">Loading...</div>
+
+var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return true;
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    
+    // Terms of service url.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    privacyPolicyUrl: '<your-privacy-policy-url>'
+  };
+
+  // The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
 
 
