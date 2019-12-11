9618b34100cbfa38751796fc1df1d58333fb86ab
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-12-11 16:31:40 -0500

    created sign up and login buttons added to app

--- components/App.js
+++ components/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './Navbar'
-
+import SignUpLogin from './SignUpLogin'
 
 
 function App () {
 
     return(
+        <div>
         <Navbar/>
+      
+      
+        <SignUpLogin/>
+
+
+        </div>
+        
     )
 }
 
---
+++ components/SignUpLogin.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+function SignUpLogin (){
+    return(
+        <div>
+
+        <ButtonToolbar>
+          <Button variant="primary" size="lg">
+            SignUp 
+          </Button>
+          <Button variant="secondary" size="lg">
+            Login
+          </Button>
+        </ButtonToolbar>
+
+        </div>
+    )
+}
+
+
+export default SignUpLogin;
