6c5e862e250f924fa48280a2d21b529706309f0a
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-12-11 16:05:04 -0500

    installed react & created app and navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Navbar(){
+    return(
+        <Container>
+        <Navbar expand="lg" variant="light" bg="light">
+          <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        </Navbar>
+      </Container>
+    )
+}
+
+export default Navbar;
