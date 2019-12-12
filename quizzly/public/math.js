e475be551fe66eb609910624581b573acce16956
jboe26 <joshrboepple@outlook.com>
2019-12-11 20:32:20 -0500

    putting questions in objects

---
+++ quizzly/public/math.js
@@ -0,0 +1,63 @@
+[
+    {
+      "id": 1,
+      "question": "Where should addition signs be placed between digits of the number 987654321 to get a total of 90?",
+      "answer1": "A: 9 + 8 + 76 + 5 + 4 + 3 + 21",
+      "correct": "9 + 8 + 7 + 6 + 54 + 3 + 2 + 1",
+      "answer2": "9 + 8 + 7 + 6 + 5 + 4 + 32 + 1"   
+    },
+
+ 
+Question 2: A girl has to be at school by 8:00 A.M. It takes her 20 minutes to eat breakfast, 35 minutes to get dressed, and 15 minutes to walk to school. What time should she get up in the morning?
+A: 7:00 A.M.
+B: 6:50 A.M.
+C: 6:40 A.M.
+D: 6:30 A.M.
+ 
+Question 3: Five hundred forty people went to the mall on Saturday. Twice as many people went to the mall on Sunday. How many people went to the mall for the entire weekend?
+A: 1,080
+B: 1,620
+C: 1,480
+D: 1,220
+ 
+Question 4: Emma has enough money to buy six compact discs per week. How many weeks until Emma can buy 30 compact discs?
+A: 5
+B: 6
+C: 7
+D: 8
+ 
+Question 5: Gerald's son will be fifteen this Friday. If Gerald just turned 40 years old, how old was Gerald when his son was born?
+A: 23
+B: 24
+C: 25
+D: 26
+ 
+Question 6: To go to the zoo, seven cars carried 28 people. If the same number of people were in each car, how many people were there per car?
+A: 7
+B: 6
+C: 5
+D: 4
+ 
+Question 7: Mindy had 15 minutes to get to work. How many seconds did she have?
+A: 60
+B: 300
+C: 900
+D: 1,200
+ 
+Question 8: Debbie went to pick apples from the orchard. She picked a bushel of 180 apples. She wanted to put them in little baskets that can each hold 9 apples. How many baskets will she need to pack the apples?
+A: 12
+B: 20
+C: 24
+D: 30
+ 
+Question 9: In the first year of production a play sells 872 tickets, in its second year it sells 1053 tickets, in its third year it sells 92 less than in its second year. How many tickets are sold in 3 years?
+A: 1,961
+B: 2,961
+C: 2,886
+D: 3,986
+ 
+Question 10: If you begin with a one digit integer, multiply by 3, add 8, divide by 2 and subtract 6, you will get the integer back. Find the number.
+A: 2
+B: 4
+C: 5
+D: 8
