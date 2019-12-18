7afc2d53b00ce181649012dfa53c34a4c8c0973f
Sheldon Perkins <csperkins18@gmail.com>
2019-12-18 16:47:39 -0500

    database schema

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO question (category_id, question) VALUES ('History', 'Which war was fought between the north and the south regions of the United States?');
+Insert Into question_choices (question_id, right_choice, choice) Values (1, 1, 'Civil War')
+Insert Into question_choices (question_id, right_choice, choice) Values (1, 0, 'Mexican War')
+Insert Into question_choices (question_id, right_choice, choice) Values (1, 0, 'World War I')
+Insert Into question_choices (question_id, right_choice, choice) Values (1, 0, 'World War II')
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require("express");
+var path = require("path");
 
 var PORT = process.env.PORT || 3000;
 
 var app = express();
+const mysql = require('mysql');
+// TO DO set up View engine
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -10,16 +14,6 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-var exhb = require("express-handlebars");
-
-app.engine("handlebars", exhb({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
-
-app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
